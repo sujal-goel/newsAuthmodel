aa86fce1a9f54cada395c13ec1850307e5efaf00
sujal goel <sujalgoel19@gmail.com>
2025-05-14 01:03:18 +0530

    changes from js to ts

--- api/index.js
+++ api/index.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
   const label = predictionData[0] > 0.5 ? 'REAL' : 'FAKE';
   res.status(200).json({ prediction: label, confidence: predictionData[0].toFixed(2) });
 }
-
+app.get("/",(res)=>{res.send("apiRunningSuccessfully")})
 // Use the handler for POST requests
 app.post('/predict', handler);
 
